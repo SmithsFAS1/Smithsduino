1eded0dec2e94242848e75981ab2001e575bfb46
SmithsFAS1 <git.nospamming@xoxy.net>
2023-05-05 17:39:44 -0400

    Revert "Updated for latest IRRemote Library"
    This reverts commit 6dd2fb870cb977ec2fdee474aec858c16afb41e7.

--- XB360/OTA.h
+++ XB360/OTA.h
@@ -10,60 +10,51 @@ String formatBytes(size_t bytes) {
   }
 }
 
-const char OTAindex[] PROGMEM = R"rawliteral(
-  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-  <u>Firmware Upload</u><br>
-  <form method='POST' action='#' enctype='multipart/form-data' id='upload_form' style='padding:10px;'>
-    <input type='file' name='update'>
-    <input type='submit' value='Update'>
-  </form>
-  <div id='prg'>Select file and click "Update"</div>
-  <script>
-    $('form').submit(function(e){
-    e.preventDefault();
-    var form = $('#upload_form')[0];
-    var data = new FormData(form);
-    $.ajax({
-      url: '/update',
-      type: 'POST',
-      data: data,
-      contentType: false,
-      processData:false,
-      xhr: function() {
-        var xhr = new window.XMLHttpRequest();
-        xhr.upload.addEventListener('progress', function(evt) {
-          if (evt.lengthComputable) {
-            var per = evt.loaded / evt.total;
-            $('#prg').html('progress: ' + Math.round(per*100) + '%');
-          }
-        }, false);
-      return xhr;
-      },
-        success:function(d, s) {
-          $('#prg').html('SUCCESS, RESTARTING');
-          setTimeout(function(){window.location.reload();
-        }, 5000);
-      },
-      error: function (a, b, c) {
-      }
-    });
-  });
-  </script>
-   )rawliteral";
 
-const char logoutHTML[] PROGMEM = R"rawliteral(
-  <script>
-  function logoutButton() {
-    if (confirm("Log out?") == true) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET',window.location.protocol + '//log:out@' + window.location.hostname + '/logout', true);
-      xhr.send();
-      setTimeout(function(){ window.open('/logged-out','_self'); }, 1000);
-    }
-  }
-  </script>
-  <div><button onclick="logoutButton();">Logout</button></div>
-  )rawliteral";
+const char* serverIndex =
+  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+  "<input type='file' name='update'>"
+  "<input type='submit' value='Update'>"
+  "</form><p>"
+  "<div id='prg'>Select file and click \"Update\"</div>"
+  "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "$('#prg').html('SUCCESS, RESTARTING');"
+  "setTimeout(function(){window.location.reload();"
+  "}, 5000);"
+  "},"
+  "error: function (a, b, c) {"
+  "}"
+  "});"
+  "});"
+  "function logoutButton() {"
+  "var xhr = new XMLHttpRequest();"
+  "xhr.open('GET',window.location.protocol + '//log:out@' + window.location.hostname + '/logout', true);"
+  "xhr.send();"
+  "setTimeout(function(){ window.open('/logged-out','_self'); }, 1000);"
+  "}"
+  "</script>";
 
 const char logout_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
